3a2916d76fd9b6de3167afe937e5f1e889e74e52
auror7269 <mohammad.209302121@muj.manipal.edu>
2023-06-10 15:34:17 +0530

    Added

---
+++ Intersection_Of_2_LL.cpp
@@ -0,0 +1,38 @@
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+Node* findIntersection(Node *firstHead, Node *secondHead)
+{
+    if(firstHead==NULL || secondHead==NULL) return NULL;
+    Node *a=firstHead;
+    Node *b=secondHead;
+    while(a!=b){
+        a = a==NULL? secondHead: a->next;
+        b = b==NULL? firstHead: b->next;
+    }
+    if (a != NULL) {
+      return a;
+    }
+    return NULL;
+}
